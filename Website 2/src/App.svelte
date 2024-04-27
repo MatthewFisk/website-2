420cfb2a090d61043207cb42d4eea46319844fcd
mahttchoo <56457050+mahttchoo@users.noreply.github.com>
2024-04-26 22:49:44 -0700

    Worked on second version of project cards
    they expand smoothly now, text fades in nicely, they all line up correctly.

--- Website 2/src/App.svelte
+++ Website 2/src/App.svelte
@@ -1,10 +1,9 @@
 <style>
-  #projects {
+  .projects {
     display:block;
-    max-width:1000px;
+    max-width:1200px;
     text-align:center;
     margin:0 auto;
-    margin-bottom: 1000px;
 }
 </style>
 
@@ -12,37 +11,100 @@
   import LandingHeader from "./lib/LandingHeader.svelte";
   import SearchBar from "./lib/SearchBar.svelte";
   import ProjectCard from "./lib/ProjectCard.svelte";
+  import ProjectCardV1 from "./lib/ProjectCardV1.svelte";
   let src = "./src/assets/placeholder.webp";
 </script>
 
 <main>
   <LandingHeader />
-  
-  <div id='projects'>
+  <div class='projects'>
     <h1>Projects</h1>
     <ProjectCard project_name="Falcon Racing &nbsp &nbsp Driver Interface"
                  src='./src/assets/falcon-racing-driver-interface.png'
-                 tech_stack={['raspberry-pi', 'python', 'tkinter', 'sqlite']}/>
+                 tech_stack={['raspberry-pi', 'python', 'tkinter', 'sqlite']}
+                 scope={4}
+                 team_size={3}
+                 description="Was a member of an automotive engineering club. Used a
+                              Raspberry Pi 4 to hook up several sensors to the car, displaying
+                              the current gear, speed, and current fuel levels."/>
     <ProjectCard project_name="JuiceBox"
                  src='./src/assets/placeholder.webp'
-                 tech_stack={['rust', 'bevy']}/>
+                 tech_stack={['rust', 'bevy']}
+                 scope={4}
+                 team_size={4}
+                 description="Real-time 2D fluid simulation. Senior software engineering
+                              project."/>
     <ProjectCard project_name="Terrier &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Web Front-End"
                  src='./src/assets/terrier-web-frontend.png'
-                 tech_stack={['react', 'javascript', 'vite', 'html', 'css', 'figma']}/>
+                 tech_stack={['react', 'javascript', 'vite', 'html', 'css', 'figma']}
+                 scope={3}
+                 team_size={1}
+                 description='Internship project for Wet Dog Weather. Built them a web
+                              frontend for their "Terrier" weather graphics tool.'/>
     <ProjectCard project_name="Mock-up &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp System Proposal"
                  src='./src/assets/mock-up-system-proposal.jpg'
-                 tech_stack={[]}/>
+                 tech_stack={[]}
+                 scope={2}
+                 team_size={1}
+                 description="Mock-up technical document for a hypothetical proposed software
+                              product. Written for my System Design Class."/>
     <ProjectCard project_name="Orbit Simulator"
                  src='./src/assets/orbit-simulator.png'
-                 tech_stack={['javascript', 'html', 'css']}/>
+                 tech_stack={['javascript', 'html', 'css']}
+                 scope={1}
+                 team_size={1}
+                 description="Orbital Simulation that runs in the browser. Made for fun."/>
     <ProjectCard project_name="Courseflow Generator"
                  src='./src/assets/course-flowchart-generator.png'
-                 tech_stack={['cpp']}/>
+                 tech_stack={['cpp']}
+                 scope={2}
+                 team_size={2}
+                 description="Uses graph theory to construct an optimal course sequence given
+                              a list of required courses. Final project for my Algorithm Design
+                              and Analysis class."/>
     <ProjectCard project_name="Graph Library"
                  src='./src/assets/java-graph-library.png'
-                 tech_stack={['java']}/>
+                 tech_stack={['java']}
+                 scope={1}
+                 team_size={1}
+                 description="Graph data structure library. Implements basic use cases like
+                              vertices, undirected edges, directed edges, and edge weights
+                              as well as some common search algorithms. Made as a self-study."/>
     <ProjectCard project_name="RC Fireboat"
                  {src}
-                 tech_stack={['raspberry-pi', 'python', 'micro-python']}/>
+                 tech_stack={['raspberry-pi', 'python', 'micro-python']}
+                 scope={2}
+                 team_size={4}
+                 description="Used a Raspberry Pi Pico H to build a remote controlled fireboat.
+                              It uses [BT/Wifi/IR] and is able to go forwards, backwards, steer
+                              left, steer right, and shoot a water cannon. Made for my Microcontroller
+                              System Design class."/>
+  </div>
+  <div class='projects'>
+    <h1>Projects</h1>
+    <ProjectCardV1 project_name="Falcon Racing &nbsp &nbsp Driver Interface"
+                   src='./src/assets/falcon-racing-driver-interface.png'
+                  tech_stack={['raspberry-pi', 'python', 'tkinter', 'sqlite']}/>
+<ProjectCardV1 project_name="JuiceBox"
+    src='./src/assets/placeholder.webp'
+    tech_stack={['rust', 'bevy']}/>
+<ProjectCardV1 project_name="Terrier &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Web Front-End"
+    src='./src/assets/terrier-web-frontend.png'
+    tech_stack={['react', 'javascript', 'vite', 'html', 'css', 'figma']}/>
+<ProjectCardV1 project_name="Mock-up &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp System Proposal"
+    src='./src/assets/mock-up-system-proposal.jpg'
+    tech_stack={[]}/>
+<ProjectCardV1 project_name="Orbit Simulator"
+    src='./src/assets/orbit-simulator.png'
+    tech_stack={['javascript', 'html', 'css']}/>
+<ProjectCardV1 project_name="Courseflow Generator"
+    src='./src/assets/course-flowchart-generator.png'
+    tech_stack={['cpp']}/>
+<ProjectCardV1 project_name="Graph Library"
+    src='./src/assets/java-graph-library.png'
+    tech_stack={['java']}/>
+<ProjectCardV1 project_name="RC Fireboat"
+    {src}
+    tech_stack={['raspberry-pi', 'python', 'micro-python']}/>
   </div>
 </main>
--- Website 2/src/lib/ProjectCard.svelte
+++ Website 2/src/lib/ProjectCard.svelte
@@ -1,11 +1,54 @@
-<style>
+<!--
+    TODO:
+    Main card should scale down when expanded card increases
+    Card Expanded 
+-->
 
+<script>
+    export let project_name = "default";
+    export let src = "./src/assets/placeholder.jpg";
+    export let tech_stack = ["java", "python"];
+    export let scope = 1;
+    export let team_size = 1;
+    export let description = "This project was fun!";
+</script>
+
+<div class="card_positioner">
+    <div class="card_expand">
+        <div class="card_expand_upper">
+            <!-- Base Card -->
+            <div class="card">
+                <img {src} class="card_image" />
+                <div class="blur" />
+                <h2>{project_name}</h2>
+                <div class="tech_stack">
+                    {#each tech_stack as image_name}
+                        <img src="./src/assets/{image_name}.png" />
+                    {/each}
+                </div>
+            </div>
+
+            <!-- Right Expansion -->
+            <div class='card_expand_right'>
+                <p>{description}</p>
+            </div>
+        </div>
+
+        <div class="card_expand_bottom">
+            <h3>Scope: {scope}</h3>
+            <h3>Team Size: {team_size}</h3>
+        </div>
+    </div>
+</div>
+
+<style>
     /* Project Name */
     h2 {
         width: 205px; /* No wider than the image */
-        
+
         position: absolute;
-        bottom: 50px;
+        transform: translateY(-100%);
+        top: 195px;
         left: 15px;
 
         text-wrap: wrap;
@@ -20,45 +63,12 @@
 
         width: 225px;
         height: 285px;
-        margin: 10px;
 
-        position: relative;
+        align-content: left;
 
         display: inline-block;
-        vertical-align: top;
 
         border-radius: 15px;
-        box-shadow: 0px 0px 10px 2px #2e2d2f;
-    }
-    .card:hover {
-        background-color: #6a686d;
-    }
-    .card_expanded {
-        background-color: #444445;
-
-        position: relative;
-        right: 112.5px;
-        bottom: 295px;
-        z-index: 99;
-
-        overflow: hidden;
-        
-        border-radius: 15px;
-        box-shadow: 0px 0px 40px 5px black;
-
-        /* height, overflow, scale make it invisible before hover. */
-        height: 0px;
-        overflow: hidden;
-        transform: scale(0);
-        transform-origin: center 120px;
-        transition: transform 0.15s ease-in-out;
-    }
-    .card:hover .card_expanded {
-        width: 450px;           /* Width and height unaffected by transition time. */
-        height: 350px;
-
-        transform: scale(1);    /* Transition from scale(0) -> scale(1) makes .card_expanded pop out from the .card. */
-        transition-delay: 0.3s; /* Gives some delay when the user hovers over the card before popping out the expanded view so we're sure the user really wants to see it. */
     }
 
     /* Main images */
@@ -71,22 +81,15 @@
         border-top-left-radius: 15px;
         border-top-right-radius: 15px;
     }
-    .expanded_card_image {
-        width: 175px;
-        height: 175px;
-        margin: 10px;
-
-        position: relative;
-        top: 0px;
-        left: 0px;
-
-        display: block;
-
-        border-top-left-radius: 15px;
-        align-items: start;
-    }
     .blur {
-        background: rgba(0, 0, 0, 0) linear-gradient(to bottom, rgba(0, 0, 0, 0) 10%, rgba(0, 0, 0, .1) 40%, rgba(0, 0, 0, .5) 65%, rgba(0, 0, 0, 1) 100%);
+        background: rgba(0, 0, 0, 0)
+            linear-gradient(
+                to bottom,
+                rgba(0, 0, 0, 0) 10%,
+                rgba(0, 0, 0, 0.1) 40%,
+                rgba(0, 0, 0, 0.5) 65%,
+                rgba(0, 0, 0, 1) 100%
+            );
 
         width: 215px;
         height: 200px;
@@ -99,12 +102,11 @@
 
     /* Tech Stack */
     .tech_stack {
-        height: 35px;           /* Ensures that .tech_stack takes up room even if there are no images */
+        height: 35px; /* Ensures that .tech_stack takes up room even if there are no images */
         margin-top: 5px;
         padding-left: 10px;
         display: block;
         text-align: left;
-        
     }
     .tech_stack img {
         height: 35px;
@@ -113,47 +115,123 @@
 
         /* White border */
         -webkit-filter: drop-shadow(1px 1px 0 white)
-                        drop-shadow(-1px 1px 0 white)
-                        drop-shadow(1px -1px 0 white)
-                        drop-shadow(-1px -1px 0 white);
-        filter: drop-shadow(1px 1px 0 white)
-                drop-shadow(-1px 1px 0 white)
-                drop-shadow(1px -1px 0 white)
-                drop-shadow(-1px -1px 0 white);
+            drop-shadow(-1px 1px 0 white) drop-shadow(1px -1px 0 white)
+            drop-shadow(-1px -1px 0 white);
+        filter: drop-shadow(1px 1px 0 white) drop-shadow(-1px 1px 0 white)
+            drop-shadow(1px -1px 0 white) drop-shadow(-1px -1px 0 white);
     }
 
+    /* Card Expansion and Position */
+    .card_positioner {
+        width: 225px;
+
+        position: relative;
+
+        display: inline-block;
+        vertical-align: top;
+
+        margin: 10px;
+        margin-bottom: 335px;
+    }
+    .card_expand {
+        background-color: #444445;
+
+        width: 225px;
+
+        position: absolute;
+        left: -10px;
+        margin: 15px;
+
+        display: inline-block;
+        vertical-align: top;
+        align-content: start;
+
+        border-radius: 15px;
+        box-shadow: 0px 0px 10px 2px #2e2d2f;
+
+        transition:
+            width 0.15s ease-in-out,
+            transform 0.15s ease-in-out;
+    }
+    .card_expand_upper {
+        display: inline-flex;
+    }
+    .card_expand_right {
+        height: 0px;
+
+        padding: 10px;
+
+        justify-content: center;
+    }
+    .card_expand_right p {
+        pointer-events: none;
+        opacity: 0;
+        transition: opacity 0.1s ease-out;
+    }
+    .card_expand_bottom {
+        background-color: #444445;
+
+        height: 0px;
+
+        border-radius: 15px;
+
+        transition: height 0.15s ease-in-out;
+    }
+    .card_expand_bottom h3 {
+        margin: 0px;
+
+        opacity: 0;
+
+        transition: opacity 0.1s ease-out;
+    }
+
+    /* Card Transitions on Hover */
+    .card_expand:hover {
+        width: 450px;
+
+        transform: translateX(-25%) translateY(-25px); /* Centering the expanded card */
+        z-index: 99;
+
+        box-shadow: 0px 0px 40px 5px black;
+
+        transition-delay: 0.3s;
+    }
+    .card_expand:hover .card_expand_upper {
+        width: 450px;
+        transition-delay: 0.3s;
+    }
+    .card_expand:hover .card_expand_upper .card_expand_right {
+        width: 225px;
+    }
+    .card_expand:hover .card_expand_upper .card_expand_right p {
+        opacity: 1;
+        transition: opacity 0.45s ease-in;
+        transition-delay: 0.3s;
+    }
+    .card_expand:hover .card_expand_bottom {
+        height: 100px;
+        transition-delay: 0.3s;
+    }
+    .card_expand:hover .card_expand_bottom h3 {
+        opacity: 1;
+        transition: opacity 0.45s ease-in;
+        transition-delay: 0.3s;
+    }
+    .card_expand:hover .card img {
+        border-top-right-radius: 0px;
+        transition-delay: 0.3s;
+    }
+    .card_expand:hover .card {
+        /*transform: scale(0.7);*/
+        animation: lighten 0.35s 1;
+        animation-timing-function: steps(1, end);
+    }
+    @keyframes lighten {
+        0% {
+            background-color: #6a686d;
+        }
+        100% {
+            background-color: #444445;
+        }
+    }
 </style>
-
-<script>
-    export let project_name = "default";
-    export let src = './src/assets/placeholder.jpg';
-    export let tech_stack = ['java', 'python'];
-    export let scope = 1;
-    export let team_size = 1;
-</script>
-
-<div class='card'>
-
-    <!-- Base Card -->
-    <img {src} class='card_image'/>
-    <div class='blur' />
-    <h2>{project_name}</h2>
-    <div class='tech_stack'>
-        {#each tech_stack as image_name}
-            <img src='./src/assets/{image_name}.png'>
-        {/each}
-    </div>
-
-    <!-- Expanded Card (Hidden by Default) -->
-    <div class='card_expanded'>
-        <img {src} class='expanded_card_image'/>
-        <div class='tech_stack'>
-            {#each tech_stack as image_name}
-                <img src='./src/assets/{image_name}.png'>
-            {/each}
-        </div>
-        <h3>Scope: {scope}</h3>
-        <p>Team Size: {team_size}</p>
-    </div>
-
-</div>
